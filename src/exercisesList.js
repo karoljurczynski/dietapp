ff08febd5833fc9739209316e9e0e3e5cf6fd616
Karol <karol_jurczynski@o2.pl>
2021-04-21 14:13:01 +0200

    Fixed bugs in settings backup, added settings for training section

---
+++ src/exercisesList.js
@@ -0,0 +1,136 @@
+export const exercises = [
+  { id: 0,
+    name: "Bench Press",
+    description: `The bench press is an upper-body weight training exercise in which the trainee
+                  presses a weight upwards while lying on a weight training bench. 
+                  The exercise uses the pectoralis major, the anterior deltoids, and the triceps,
+                  among other stabilizing muscles. A barbell is generally used to hold the weight, 
+                  but a pair of dumbbells can also be used.`,
+    difficulty: 2, 
+    typeOfExercise: "Compound exercise",
+    muscles: ["Chest", "Shoulders", "Arms"],
+    properFormLink: "https://www.youtube.com/embed/XSza8hVTlmM" 
+  },
+
+  { id: 1,
+    name: "Deadlift",
+    description: `Deadlift refers to the lifting of dead weight (weight without momentum),
+                  such as weights lying on the ground. It is one of the few standard weight training exercises
+                  in which all repetitions begin with dead weight. In most other lifts, there is an eccentric 
+                  (lowering of the weight) phase followed by the concentric (lifting of the weight) phase. 
+                  During these exercises, a small amount of energy is stored in the stretched muscles and 
+                  tendons in the eccentric phase if the lifter is not flexible beyond the range of motion.
+                  Although this exercise uses the legs and hips as the primary movers, it can also be considered a back exercise.`,
+    difficulty: 3, 
+    typeOfExercise: "Compound exercise",
+    muscles: ["Legs", "Back", "Core"],
+    properFormLink: "https://www.youtube.com/embed/op9kVnSso6Q" 
+  },
+
+  { id: 2,
+    name: "Squat",
+    description: `A squat is a strength exercise in which the trainee lowers their hips from a standing position
+                  and then stands back up. During the descent of a squat, the hip and knee joints flex while
+                  the ankle joint dorsiflexes; conversely the hip and knee joints extend and the ankle joint
+                  plantarflexes when standing up. Squats are considered a vital exercise for increasing the strength and size of the lower body
+                  muscles as well as developing core strength. The primary agonist muscles used during the squat are
+                  the quadriceps femoris, the adductor magnus, and the gluteus maximus.`,
+    difficulty: 3, 
+    typeOfExercise: "Compound exercise",
+    muscles: ["Legs", "Core"],
+    properFormLink: "https://www.youtube.com/embed/ultWZbUMPL8"
+  },
+
+  { id: 3,
+    name: "Pull-up",
+    description: `A pull-up is an upper-body strength exercise. The pull-up is a closed-chain movement where 
+                  the body is suspended by the hands and pulls up. As this happens, the elbows 
+                  flex and the shoulders adduct and extend to bring the elbows to the torso.`,
+    difficulty: 2, 
+    typeOfExercise: "Compound exercise",
+    muscles: ["Upper back", "Arms"],
+    properFormLink: "https://www.youtube.com/embed/HRV5YKKaeVw"
+  },
+
+  { id: 4,
+    name: "Push-up",
+    description: `A push-up is a common calisthenics exercise beginning from the prone position. 
+                  By raising and lowering the body using the arms, push-ups exercise the pectoral muscles, 
+                  triceps, and anterior deltoids, with ancillary benefits to the rest of the deltoids, 
+                  serratus anterior, coracobrachialis and the midsection as a whole. Push-ups are a basic 
+                  exercise used in civilian athletic training or physical education and commonly in military 
+                  physical training.`,
+    difficulty: 1, 
+    typeOfExercise: "Compound exercise",
+    muscles: ["Chest", "Shoulders", "Arms", "Core"],
+    properFormLink: "https://www.youtube.com/embed/0pkjOk0EiAk"
+  },
+
+  { id: 5,
+    name: "Overhead Press",
+    description: `The overhead press (abbreviated OHP), also referred to as a shoulder press, military press,
+                  or simply the press, is a weight training exercise with many variations. It is typically performed
+                  while either standing or sitting sometimes also when squatting, in which a weight is pressed straight
+                  upwards from racking position until the arms are locked out overhead, while the legs, lower back and
+                  abs maintain balance.[1] The exercise helps build muscular shoulders with bigger arms, and is one
+                  of the most difficult compound upper-body exercises.`,
+    difficulty: 2, 
+    typeOfExercise: "Compound exercise",
+    muscles: ["Shoulders", "Arms", "Core"],
+    properFormLink: "https://www.youtube.com/embed/2yjwXTZQDDI"
+  },
+
+  { id: 6,
+    name: "Bent-over Row",
+    description: `A bent-over row (or barbell row) is a weight training exercise 
+                  that targets a variety of back muscles. Which ones are targeted varies on form. 
+                  The bent over row is often used for both bodybuilding and powerlifting. 
+                  It is a good exercise for increasing strength and size`,
+    difficulty: 2, 
+    typeOfExercise: "Compound exercise",
+    muscles: ["Back", "Arms", "Core"],
+    properFormLink: "https://www.youtube.com/embed/kBWAon7ItDw"
+  },
+
+  { id: 7,
+    name: "Biceps Curl",
+    description: `Biceps curl is a general term for a series of strength exercises 
+                  that involve brachioradialis, front deltoid and the main target on biceps brachii.
+                  Includes variations using barbell, dumbbell and resistance band, etc. The common point
+                  amongst them is the trainee lifting a certain amount of weight to contracting the biceps
+                  brachii, and tuck in their arms to the torso during the concentric phase. Once the biceps brachii
+                  is fully contracted, then return the weight to starting position during the eccentric phase.`,
+    difficulty: 1, 
+    typeOfExercise: "Isolated exercise",
+    muscles: ["Biceps", "Arms"],
+    properFormLink: "https://www.youtube.com/embed/ykJmrZ5v0Oo"
+  },
+
+  { id: 8,
+    name: "French Press",
+    description: `Lying triceps extensions, also known as skull crushers and French extensions
+                  or French presses, are a strength exercise used in many different forms of strength training.
+                  Lying triceps extensions are one of the most stimulating exercises to the entire triceps muscle
+                  group in the upper arm. It works the triceps from the elbow all the way to the latissimus dorsi.
+                  Due to its full use of the Triceps muscle group, the lying triceps extensions are used by many
+                  as part of their training regimen.`,
+    difficulty: 1, 
+    typeOfExercise: "Isolated exercise",
+    muscles: ["Triceps", "Arms"],
+    properFormLink: "https://www.youtube.com/embed/JImgCWzCHwI"
+  },
+
+  { id: 9,
+    name: "Running",
+    description: `Running is a method of terrestrial locomotion allowing humans and other animals
+                  to move rapidly on foot. Running is a type of gait characterized by an aerial phase in which
+                  all feet are above the ground (though there are exceptions). A feature of a running
+                  body from the viewpoint of spring-mass mechanics is that changes in kinetic and potential energy
+                  within a stride occur simultaneously, with energy storage accomplished by springy tendons and
+                  passive muscle elasticity. The term running can refer to any of a variety of speeds ranging from jogging to sprinting. `,
+    difficulty: 1, 
+    typeOfExercise: "Aerobic exercise",
+    muscles: ["Legs", "Core"],
+    properFormLink: "https://www.youtube.com/embed/brFHyOtTwH4"
+  }
+]
--- src/index.js
+++ src/index.js
@@ -8,6 +8,7 @@ import DateChanger from './components/center/DateChanger';
 import Meal from './components/meal/Meal';
 import Gauge from './components/right/Gauge';
 import Exercise from './components/exercise/Exercise';
+import { exercises } from './exercisesList';
 
 import './styles/index/index.css';
 import './components/left/styles/left.css';
@@ -25,10 +26,11 @@ const ACTIONS = {
   CHANGE_DATE: 'change-date',
   CHANGE_PAGE_TITLE: 'change-page-title',
   CHANGE_SETTINGS_DATA: 'change-settings-data',
-  BACKUP_OLD_SETTINGS: 'backup-old-settings',
-  RESTORE_OLD_SETTINGS: 'restore-old-settings',
   LOAD_SETTINGS: 'load-settings',
-  SET_CLEAR_ALL_PRODUCTS: 'set-clear-all-products'
+  SET_CLEAR_ALL_PRODUCTS: 'set-clear-all-products',
+  ADD_EXERCISE_TO_SELECTEDEXERCISES: 'add-exercise-to-selectedexercises',
+  REMOVE_EXERCISE_FROM_SELECTEDEXERCISES: 'remove-exercise-from-selectedexercises',
+  SET_SETTINGS_CHANGED_STATE: 'set-settings-changed-state'
 }
 
 
@@ -61,7 +63,6 @@ function App() {
     else
       saveSettingsToLocalStorage(); 
 
-    backupSettings();
   }, []);
 
   const reducer = (state, action) => {
@@ -172,6 +173,33 @@ function App() {
         return { ...state, clearAllProducts: action.payload };
       }
 
+      case ACTIONS.ADD_EXERCISE_TO_SELECTEDEXERCISES: {
+        const updatedSelectedExercises = state.settingsData.training.selectedExercises;
+        updatedSelectedExercises.push(action.payload);
+
+        return {...state, 
+          settingsData: { ...state.settingsData, 
+          training: {...state.settingsData.training,
+          selectedExercises: updatedSelectedExercises }}
+        };
+      }
+
+      case ACTIONS.REMOVE_EXERCISE_FROM_SELECTEDEXERCISES: {
+        const updatedSelectedExercises = state.settingsData.training.selectedExercises;
+        const indexOfExerciseToRemoving = updatedSelectedExercises.indexOf(action.payload);
+        updatedSelectedExercises.splice(indexOfExerciseToRemoving, 1);
+
+        return {...state, 
+          settingsData: { ...state.settingsData, 
+          training: {...state.settingsData.training,
+          selectedExercises: updatedSelectedExercises }}
+        }
+      }
+
+      case ACTIONS.SET_SETTINGS_CHANGED_STATE: {
+        return { ...state, isSettingsChanged: action.payload };
+      }
+
       default: return console.error(`Unknown action type: ${action.type}`);
     }
   }
@@ -199,151 +227,28 @@ function App() {
       },
 
       training: {
-        exercises: [
-          { id: 0,
-            name: "Bench Press",
-            description: `The bench press is an upper-body weight training exercise in which the trainee
-                          presses a weight upwards while lying on a weight training bench. 
-                          The exercise uses the pectoralis major, the anterior deltoids, and the triceps,
-                          among other stabilizing muscles. A barbell is generally used to hold the weight, 
-                          but a pair of dumbbells can also be used.`,
-            difficulty: 2, 
-            typeOfExercise: "Compound exercise",
-            muscles: ["Chest", "Shoulders", "Arms"],
-            properFormLink: "https://www.youtube.com/embed/XSza8hVTlmM" 
-          },
-
-          { id: 1,
-            name: "Deadlift",
-            description: `Deadlift refers to the lifting of dead weight (weight without momentum),
-                          such as weights lying on the ground. It is one of the few standard weight training exercises
-                          in which all repetitions begin with dead weight. In most other lifts, there is an eccentric 
-                          (lowering of the weight) phase followed by the concentric (lifting of the weight) phase. 
-                          During these exercises, a small amount of energy is stored in the stretched muscles and 
-                          tendons in the eccentric phase if the lifter is not flexible beyond the range of motion.
-                          Although this exercise uses the legs and hips as the primary movers, it can also be considered a back exercise.`,
-            difficulty: 3, 
-            typeOfExercise: "Compound exercise",
-            muscles: ["Legs", "Back", "Core"],
-            properFormLink: "https://www.youtube.com/embed/op9kVnSso6Q" 
-          },
-
-          { id: 2,
-            name: "Squat",
-            description: `A squat is a strength exercise in which the trainee lowers their hips from a standing position
-                          and then stands back up. During the descent of a squat, the hip and knee joints flex while
-                          the ankle joint dorsiflexes; conversely the hip and knee joints extend and the ankle joint
-                          plantarflexes when standing up. Squats are considered a vital exercise for increasing the strength and size of the lower body
-                          muscles as well as developing core strength. The primary agonist muscles used during the squat are
-                          the quadriceps femoris, the adductor magnus, and the gluteus maximus.`,
-            difficulty: 3, 
-            typeOfExercise: "Compound exercise",
-            muscles: ["Legs", "Core"],
-            properFormLink: "https://www.youtube.com/embed/ultWZbUMPL8"
-          },
-
-          { id: 3,
-            name: "Pull-up",
-            description: `A pull-up is an upper-body strength exercise. The pull-up is a closed-chain movement where 
-                          the body is suspended by the hands and pulls up. As this happens, the elbows 
-                          flex and the shoulders adduct and extend to bring the elbows to the torso.`,
-            difficulty: 2, 
-            typeOfExercise: "Compound exercise",
-            muscles: ["Upper back", "Arms"],
-            properFormLink: "https://www.youtube.com/embed/HRV5YKKaeVw"
-          },
-
-          { id: 4,
-            name: "Push-up",
-            description: `A push-up is a common calisthenics exercise beginning from the prone position. 
-                          By raising and lowering the body using the arms, push-ups exercise the pectoral muscles, 
-                          triceps, and anterior deltoids, with ancillary benefits to the rest of the deltoids, 
-                          serratus anterior, coracobrachialis and the midsection as a whole. Push-ups are a basic 
-                          exercise used in civilian athletic training or physical education and commonly in military 
-                          physical training.`,
-            difficulty: 1, 
-            typeOfExercise: "Compound exercise",
-            muscles: ["Chest", "Shoulders", "Arms", "Core"],
-            properFormLink: "https://www.youtube.com/embed/0pkjOk0EiAk"
-          },
-
-          { id: 5,
-            name: "Overhead Press",
-            description: `The overhead press (abbreviated OHP), also referred to as a shoulder press, military press,
-                          or simply the press, is a weight training exercise with many variations. It is typically performed
-                          while either standing or sitting sometimes also when squatting, in which a weight is pressed straight
-                          upwards from racking position until the arms are locked out overhead, while the legs, lower back and
-                          abs maintain balance.[1] The exercise helps build muscular shoulders with bigger arms, and is one
-                          of the most difficult compound upper-body exercises.`,
-            difficulty: 2, 
-            typeOfExercise: "Compound exercise",
-            muscles: ["Shoulders", "Arms", "Core"],
-            properFormLink: "https://www.youtube.com/embed/2yjwXTZQDDI"
-          },
-
-          { id: 6,
-            name: "Bent-over Row",
-            description: `A bent-over row (or barbell row) is a weight training exercise 
-                          that targets a variety of back muscles. Which ones are targeted varies on form. 
-                          The bent over row is often used for both bodybuilding and powerlifting. 
-                          It is a good exercise for increasing strength and size`,
-            difficulty: 2, 
-            typeOfExercise: "Compound exercise",
-            muscles: ["Back", "Arms", "Core"],
-            properFormLink: "https://www.youtube.com/embed/kBWAon7ItDw"
-          },
-
-          { id: 7,
-            name: "Biceps Curl",
-            description: `Biceps curl is a general term for a series of strength exercises 
-                          that involve brachioradialis, front deltoid and the main target on biceps brachii.
-                          Includes variations using barbell, dumbbell and resistance band, etc. The common point
-                          amongst them is the trainee lifting a certain amount of weight to contracting the biceps
-                          brachii, and tuck in their arms to the torso during the concentric phase. Once the biceps brachii
-                          is fully contracted, then return the weight to starting position during the eccentric phase.`,
-            difficulty: 1, 
-            typeOfExercise: "Isolated exercise",
-            muscles: ["Biceps", "Arms"],
-            properFormLink: "https://www.youtube.com/embed/ykJmrZ5v0Oo"
-          },
-
-          { id: 8,
-            name: "French Press",
-            description: `Lying triceps extensions, also known as skull crushers and French extensions
-                          or French presses, are a strength exercise used in many different forms of strength training.
-                          Lying triceps extensions are one of the most stimulating exercises to the entire triceps muscle
-                          group in the upper arm. It works the triceps from the elbow all the way to the latissimus dorsi.
-                          Due to its full use of the Triceps muscle group, the lying triceps extensions are used by many
-                          as part of their training regimen.`,
-            difficulty: 1, 
-            typeOfExercise: "Isolated exercise",
-            muscles: ["Triceps", "Arms"],
-            properFormLink: "https://www.youtube.com/embed/JImgCWzCHwI"
-          },
-
-          { id: 9,
-            name: "Running",
-            description: `Running is a method of terrestrial locomotion allowing humans and other animals
-                          to move rapidly on foot. Running is a type of gait characterized by an aerial phase in which
-                          all feet are above the ground (though there are exceptions). A feature of a running
-                          body from the viewpoint of spring-mass mechanics is that changes in kinetic and potential energy
-                          within a stride occur simultaneously, with energy storage accomplished by springy tendons and
-                          passive muscle elasticity. The term running can refer to any of a variety of speeds ranging from jogging to sprinting. `,
-            difficulty: 1, 
-            typeOfExercise: "Aerobic exercise",
-            muscles: ["Legs", "Core"],
-            properFormLink: "https://www.youtube.com/embed/brFHyOtTwH4"
-          }
-        ]
+        selectedExercises: [0, 1, 2, 3, 5]
       }
     },
     clearAllProducts: false,
-    oldSettingsData: {}
+    clearAllSeries: false,
+    isSettingsChanged: false
   }
 
   const [state, dispatch] = useReducer(reducer, initialState);
 
   useEffect(() => { updateGauges() }, [ state.dateIds ]);
+
+  useEffect(() => {
+    const localStorageSettings = localStorage.getItem("settings");
+    const currentSettings = JSON.stringify(state.settingsData);
+
+    if (localStorageSettings === currentSettings)
+      dispatch({ type: ACTIONS.SET_SETTINGS_CHANGED_STATE, payload: false });
+    else
+      dispatch({ type: ACTIONS.SET_SETTINGS_CHANGED_STATE, payload: true});
+
+  }, [ state.settingsData ]);
 
   const updateMealSummary = (object, mealId) => {
     dispatch({ type: ACTIONS.UPDATE_MEALS_INGREDIENTS_SUMMARY, payload: {data: object, mealId: mealId} });
@@ -375,7 +280,7 @@ function App() {
 
     dispatch({type: ACTIONS.CHANGE_PAGE_TITLE, payload: newPageTitle });
     dispatch({ type: ACTIONS.SET_CLEAR_ALL_PRODUCTS, payload: false });
-    restoreSettingsFromBackup();
+    restoreSettingFromLocalStorage();
   }
 
   const handleMenu = (categoryTitle) => {
@@ -390,12 +295,8 @@ function App() {
     localStorage.setItem("settings", JSON.stringify(state.settingsData));
   }
 
-  const restoreSettingsFromBackup = () => {
-    dispatch({ type: ACTIONS.RESTORE_OLD_SETTINGS });
-  }
-
-  const backupSettings = () => {
-    dispatch({ type: ACTIONS.BACKUP_OLD_SETTINGS });
+  const restoreSettingFromLocalStorage = () => {
+    dispatch({ type: ACTIONS.LOAD_SETTINGS });
   }
 
   const confirmClearAllProducts = () => {
@@ -419,8 +320,7 @@ function App() {
     if (e.target[4].checked === true)
       dispatch({ type: ACTIONS.SET_CLEAR_ALL_PRODUCTS, payload: true });
 
-    backupSettings();
-    restoreSettingsFromBackup();
+    dispatch({ type: ACTIONS.SET_SETTINGS_CHANGED_STATE, payload: false });
     saveSettingsToLocalStorage();
     resetCheckbox("clearAllProducts");
     updateGauges();
@@ -428,10 +328,16 @@ function App() {
 
   const handleSettingsCanceled = (e) => {
     e.preventDefault();
-    restoreSettingsFromBackup();
-    saveSettingsToLocalStorage();
+    restoreSettingFromLocalStorage();
     resetCheckbox("clearAllProducts");
     updateGauges();
+  }
+
+  const handleExerciseChoosing = (e) => {
+    if (e.target.checked)
+      dispatch({ type: ACTIONS.ADD_EXERCISE_TO_SELECTEDEXERCISES, payload: Number(e.target.id[e.target.id.length - 1]) });
+    else
+      dispatch({ type: ACTIONS.REMOVE_EXERCISE_FROM_SELECTEDEXERCISES, payload: Number(e.target.id[e.target.id.length - 1]) });
   }
 
   const handleSettingOnChange = (e) => {
@@ -507,20 +413,20 @@ function App() {
         
         }
 
-        { state.pageTitle === 'Training' && 
+        { state.pageTitle === 'Training' &&
 
-          state.settingsData.training.exercises.map(exercise => {
+          state.settingsData.training.selectedExercises.map(selectedExerciseId => {
             return (
               <Exercise
-                key={ exercise.id } 
-                exerciseId={ exercise.id } 
+                key={ selectedExerciseId } 
+                exerciseId={ selectedExerciseId } 
                 dateIds={ state.dateIds } 
-                name={ exercise.name } 
-                difficulty={ exercise.difficulty } 
-                description={ exercise.description } 
-                muscles={ exercise.muscles } 
-                typeOfExercise={ exercise.typeOfExercise } 
-                properFormLink={ exercise.properFormLink }>
+                name={ exercises[selectedExerciseId].name } 
+                difficulty={ exercises[selectedExerciseId].difficulty } 
+                description={ exercises[selectedExerciseId].description } 
+                muscles={ exercises[selectedExerciseId].muscles } 
+                typeOfExercise={ exercises[selectedExerciseId].typeOfExercise } 
+                properFormLink={ exercises[selectedExerciseId].properFormLink }>
               </Exercise>
             )})
         }
@@ -646,6 +552,38 @@ function App() {
                 
               </section>
 
+              <section className="center-section__main__settings__form__section">
+
+                <h2 className="center-section__main__settings__form__section__title">Training</h2>
+
+                <label htmlFor="chooseExercises">Choose exercises: </label>
+
+                <section id="chooseExercises" className="center-section__main__settings__form__section__daily-demand">
+                  { exercises.map(exercise => {
+                    return (
+                      <span key={ exercise.id } className="center-section__main__settings__form__section__input">
+                        <label htmlFor={"exercise"+ exercise.id }>{ exercise.name }: </label>
+                        <input 
+                          type="checkbox"
+                          checked={ state.settingsData.training.selectedExercises.includes(exercise.id) ? true : false } 
+                          id={"exercise"+ exercise.id }
+                          onChange={ handleExerciseChoosing }
+                          />
+                      </span>
+                    )}) 
+                  }
+                  
+                </section>
+
+                <span className="center-section__main__settings__form__section__input">
+                  <label htmlFor="clearAllSeries">Clear all series: </label>
+                  <input 
+                    type="checkbox" 
+                    id="clearAllSeries" />
+                </span>
+
+              </section>
+
               <input 
                 className="center-section__main__settings__form__submit-button"
                 type="submit" 
@@ -654,8 +592,9 @@ function App() {
 
               <button 
                 className="center-section__main__settings__form__cancel-button"
-                onClick={ handleSettingsCanceled } 
-                disabled={ state.oldSettingsData === state.settingsData ? true : false }>
+                onClick={ handleSettingsCanceled }
+                type="button" 
+                disabled={ state.isSettingsChanged ? false : true }>
                 Cancel
               </button>
 
